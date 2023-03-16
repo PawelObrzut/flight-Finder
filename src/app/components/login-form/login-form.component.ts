e67d0c356ed44b1e995eaa6e47a9f90903e5151b
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-03-16 13:30:54 +0100

    can register new Users

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
+  loginEmail!: string;
+  loginPassword!: string;
+  signUpName!: string;
+  signUpLastName!: string;
+  signUpEmail!: string;
+  signUpPassword!: string;
 
+  constructor(private apiService: ApiService) {}
+  
+  @ViewChild('signUpForm') signUpForm!: NgForm;
+
+  onInit() {}
+
+  onSubmitLogin(formValues: any) {
+    // user service to perform http request
+    console.log(formValues)
+  }
+
+  onSubmitSignUp(formValues: any) {
+    this.apiService.registerUser(formValues).subscribe(response => console.log(response));
+    this.signUpForm.resetForm();
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,6 +7,13 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private apiUrl = 'http://localhost:8080/api/flights/';
+  private userUrl = 'http://localhost:8080/users/';
+
+  registerHttpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -20,5 +27,22 @@ export class ApiService {
 
   getAllFlights(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}`)
+  }
+
+  getUser(): Observable<any> {
+    return this.http.get<any>('whatever');
+  }
+
+  registerUser(user: any): Observable<any> {
+    return this.http.post<any>(
+      `${this.userUrl}register`, 
+      { 
+        name: user.signUpName,
+        lastname: user.signUpLastName,
+        email: user.signUpEmail,
+        password: user.signUpPassword
+      }, 
+      this.registerHttpOptions
+      );
   }
 }
